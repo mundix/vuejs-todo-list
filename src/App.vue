1c9ed7664a12f1591bbadf20432fe2204ced5f40
Edmundo Pichardo <ce.pichardo@gmail.com>
2019-02-08 07:08:47 -0400

    Se App.vue style and classes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <img src="./assets/logo.png">
     <HelloWorld/>
   </div>
@@ -17,12 +17,27 @@ export default {
 </script>
 
 <style>
+
+*{
+  box-sizing: border-box;
+}
+.container {
+  max-width: 500px;
+  margin:  0 auto;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
 }
 </style>
